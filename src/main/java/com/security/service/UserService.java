43e9b8d69667f408bc23c652ec72502c4eb999fa
tejasvi galave <tejugalave2004@gmail.com>
2026-04-10 13:53:21 +0530

    add get api to fetch all users

--- src/main/java/com/security/service/UserService.java
+++ src/main/java/com/security/service/UserService.java
@@ -3,8 +3,12 @@ package com.security.service;
 import com.security.entity.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface UserService {
 
     User save(User user);
+
+    List<User> getAllUsers();
 }
